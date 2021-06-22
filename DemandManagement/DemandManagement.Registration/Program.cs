520fec1c68c0d55a6e3f1a3b44705c22b1cf9460
yasartaymaz.home <yasartaymaz@windowslive.com>
2021-06-22 20:15:12 +0300

    projeler olusturuldu, masstransit projelere eklendi. 7.2'de sorun cıktı, ornekteki gibi 6.2.1 ekledim. messagecontracts'a başlandı.

---
+++ DemandManagement/DemandManagement.Registration/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DemandManagement.Registration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
